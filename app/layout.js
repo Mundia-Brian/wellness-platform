1e9cf7215c8cb1ebd59f7dde221859c26e95b5b1
pythagora <devuser@pythagora.ai>
2025-09-25 09:04:07 +0000

    Add chatbot integration to wellness platform

--- app/layout.js
+++ app/layout.js
@@ -53,6 +53,19 @@ export default function RootLayout({ children }) {
             }} />
           </>
         )}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.BASECHATBOT_DOMAIN = "https://marketing.web-designs.store";
+            window.BASECHATBOT_ID_CHATBOT = "X9N2N-A7P5G-O0U2O-N9H2N";
+            (function(){
+              d=document;
+              s=d.createElement("script");
+              s.src="https://marketing.web-designs.store/chatbot.js";
+              s.async=1;
+              d.getElementsByTagName("head")[0].appendChild(s);
+            })();
+          `
+        }} />
       </head>
       <body className="antialiased">{children}</body>
     </html>
