29385c15d60c8192ffb61dedc1efeccd1b88d1b9
pythagora <devuser@pythagora.ai>
2025-09-25 09:37:07 +0000

    Fix chatbot with client-side component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { getBrandConfig } from '../lib/brand'
+import Chatbot from '../components/Chatbot'
 
 function generateMetadata() {
   const brand = getBrandConfig()
@@ -56,9 +57,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className="antialiased">
         {children}
-        <script dangerouslySetInnerHTML={{
-          __html: `window.BASECHATBOT_DOMAIN = "https://marketing.web-designs.store"; window.BASECHATBOT_ID_CHATBOT="X9N2N-A7P5G-O0U2O-N9H2N";(function(){d=document;s=d.createElement("script");s.src="https://marketing.web-designs.store/chatbot.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`
-        }} />
+        <Chatbot />
       </body>
     </html>
   )
