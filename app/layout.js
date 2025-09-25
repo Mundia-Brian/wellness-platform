b69c63ba8ff3c289d1ef80e4c5c375174c5dab88
pythagora <devuser@pythagora.ai>
2025-09-25 09:17:12 +0000

    Move chatbot script to head section

--- app/layout.js
+++ app/layout.js
@@ -53,13 +53,12 @@ export default function RootLayout({ children }) {
             }} />
           </>
         )}
-
-      </head>
-      <body className="antialiased">
-        {children}
         <script dangerouslySetInnerHTML={{
           __html: `window.BASECHATBOT_DOMAIN = "https://marketing.web-designs.store"; window.BASECHATBOT_ID_CHATBOT="X9N2N-A7P5G-O0U2O-N9H2N";(function(){d=document;s=d.createElement("script");s.src="https://marketing.web-designs.store/chatbot.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`
         }} />
+      </head>
+      <body className="antialiased">
+        {children}
       </body>
     </html>
   )
