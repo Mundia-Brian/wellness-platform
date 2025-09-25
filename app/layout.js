239bcc7171e34e76edcbc15dfce01ac3f09a0f67
pythagora <devuser@pythagora.ai>
2025-09-25 09:11:45 +0000

    Use exact chatbot script as provided

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css'
 import { getBrandConfig } from '../lib/brand'
-import Script from 'next/script'
 
 function generateMetadata() {
   const brand = getBrandConfig()
@@ -58,16 +57,9 @@ export default function RootLayout({ children }) {
       </head>
       <body className="antialiased">
         {children}
-        <Script id="chatbot-config" strategy="afterInteractive">
-          {`
-            window.BASECHATBOT_DOMAIN = "https://marketing.web-designs.store";
-            window.BASECHATBOT_ID_CHATBOT = "X9N2N-A7P5G-O0U2O-N9H2N";
-          `}
-        </Script>
-        <Script 
-          src="https://marketing.web-designs.store/chatbot.js" 
-          strategy="afterInteractive"
-        />
+        <script dangerouslySetInnerHTML={{
+          __html: `window.BASECHATBOT_DOMAIN = "https://marketing.web-designs.store"; window.BASECHATBOT_ID_CHATBOT="X9N2N-A7P5G-O0U2O-N9H2N";(function(){d=document;s=d.createElement("script");s.src="https://marketing.web-designs.store/chatbot.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`
+        }} />
       </body>
     </html>
   )
